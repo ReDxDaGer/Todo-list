81573100dba088844012b5fba05fb501c8ba0ef7
ReDxDaGer <yashmehrotra45@gmail.com>
2024-01-31 21:04:23 +0530

    Removed all the unnecessary imports and tags

--- src/main.jsx
+++ src/main.jsx
@@ -1,35 +1,18 @@
-import React from 'react'
-import { ChakraProvider } from '@chakra-ui/react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { extendTheme } from '@chakra-ui/react'
-import { mode } from '@chakra-ui/theme-tools'
-import { BrowserRouter } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { ChakraProvider, extendTheme, ColorModeScript } from '@chakra-ui/react';
 
-const styles ={
-  global:(props) => ({
-    body:{
-      bg:mode("gray.100","#222")(props),
-      color:mode("gray.800","whiteAlpha.900")(props)
-    }
-  })
-}
-const config = {
-  initialColorMode: 'dark',
-  useSystemColorMode: false,
-}
+const config = {initialColorMode: 'dark'}
+const theme = extendTheme({config})
 
-// 3. extend the theme
-const theme = extendTheme({ config,styles })
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ChakraProvider theme={theme}>
-        <App />
-      </ChakraProvider>
-    </BrowserRouter>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
-)
+  document.getElementById('root')
+);
